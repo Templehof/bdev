0c87e5c48229592860c70f9371f24708178e8149
Ivan Riumkin <ivri0001@gmail.com>
2022-06-10 00:03:56 +0200

    further design improvements

--- src/components/pages/routed-pages/full-tool-page.js
+++ src/components/pages/routed-pages/full-tool-page.js
@@ -9,7 +9,9 @@ function ToolPage() {
 
   useEffect(() => {
     async function fetchContent() {
-      const response = await fetch(process.env.REACT_APP_BACKEND_URL + `/tools/id/${tid}`);
+      const response = await fetch(
+        process.env.REACT_APP_BACKEND_URL + `/tools/id/${tid}`
+      );
       const data = await response.json();
       return setContent(data.data.tool[0]);
     }
@@ -18,7 +20,7 @@ function ToolPage() {
 
   return (
     <section className="page-section full-tool-page">
-       <header className="header overview-header">
+      <header className="header overview-header">
         <h2 className="logo" id="logo">
           Business<span>/</span>Dev
         </h2>
@@ -29,11 +31,17 @@ function ToolPage() {
       {content && (
         <div className="full-tool-content">
           <h1 className="full-title">{content.title}</h1>
-          <img
-            src={content.toolImage ? content.toolImage : "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1115&q=80"}
-            alt="tool"
-            className="full-image"
-          />
+          <div className="image-container">
+            <img
+              src={
+                content.toolImage
+                  ? content.toolImage
+                  : "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1115&q=80"
+              }
+              alt="tool"
+              className="full-image"
+            />
+          </div>
           <p className="full-description">
             {content.longDescription
               ? content.longDescription
@@ -43,7 +51,14 @@ function ToolPage() {
             {content.scienceURL.map((item) => {
               return (
                 <li key={content.scienceURL.indexOf(item)}>
-                  <a className="source-link-full" href={item.url} target={"_blank"} rel="noreferrer" >{item.title}</a>
+                  <a
+                    className="source-link-full"
+                    href={item.url}
+                    target={"_blank"}
+                    rel="noreferrer"
+                  >
+                    {item.title}
+                  </a>
                 </li>
               );
             })}
